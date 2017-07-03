a0265e28d6be02a1aff827303d0d74e6e5f7a42f
RaduMilici <moguay@gmail.com>
2017-07-03 22:54:22 +0300

    exercise 1 start

--- 7. Components 101/main.js
+++ 7. Components 101/main.js
@@ -3,7 +3,22 @@
  */
 
 Vue.component('task', {
-    template: '<li>task component</li>'
+    template: '<li><slot></slot></li>'
+});
+
+Vue.component('task-list', {
+    template: '<ul> <task v-for="task in tasks">{{ task.description }}</task> </ul>',
+    data() {
+        return {
+            tasks: [
+                { description: 'task1', completed: false },
+                { description: 'task2', completed: true },
+                { description: 'task3', completed: false },
+                { description: 'task4', completed: true },
+                { description: 'task5', completed: false }
+            ]
+        }
+    }
 });
 
 new Vue({
